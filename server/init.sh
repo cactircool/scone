d01152ac29c657d4004fbf9f50601ff84c9cfa96
Arjun Krishnan <arjunkrishnan410@gmail.com>
2024-11-25 20:06:19 -0600

    updated init.sh to reflect previous changes

--- server/init.sh
+++ server/init.sh
@@ -2,6 +2,8 @@
 
 CERT_DIR_PREFIX='src/certs'
 
+mkdir $CERT_DIR_PREFIX
+
 openssl genrsa -out $CERT_DIR_PREFIX/server-key.pem 2048
 openssl req -new -key $CERT_DIR_PREFIX/server-key.pem -out $CERT_DIR_PREFIX/server-csr.pem
 openssl x509 -req -in $CERT_DIR_PREFIX/server-csr.pem -signkey $CERT_DIR_PREFIX/server-key.pem -out $CERT_DIR_PREFIX/server-cert.pem
