8c4b5fc4d4d9275adf1c1bf316839bb1f4f8a31f
Arjun Krishnan <arjunkrishnan410@gmail.com>
2024-12-11 14:01:45 -0600

    fixed some stuff

--- ca/create_structure.sh
+++ ca/create_structure.sh
@@ -7,4 +7,4 @@ echo 'Fill certs/radius with your ca.pem and ca.key file used in freeradius'
 echo "I'll fill certs/https with root signed certs, replace them with a CA signed set when available"
 
 cd certs/https
-openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -sha256 -days 3650 -nodes -subj "/C=US/ST=Texas/L=Frisco/O=scone/OU=dev/CN=VeryCommonName"
+openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -sha256 -days 3650 -passout pass:YourSecurePassphrase "/C=US/ST=Texas/L=Frisco/O=scone/OU=dev/CN=VeryCommonName"
